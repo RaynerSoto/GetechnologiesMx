9bda50930ff5a44da4c0eb18e3bb86974ce40555
Rayner Alejandro Soto Martínez <raynersoto01@gmail.com>
2025-05-11 21:04:09 -0400

    Actualizando el modelo de los datos

--- src/main/java/cu/rayner/getechnologiesmx/core/model/Factura.java
+++ src/main/java/cu/rayner/getechnologiesmx/core/model/Factura.java
@@ -1,4 +1,37 @@
 package cu.rayner.getechnologiesmx.core.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "facturas",schema = "public")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Factura {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO,generator = "factura_seq")
+    @SequenceGenerator(name = "facturas_seq", sequenceName = "factura_seq_id",initialValue = 1,allocationSize = 1)
+    @Column(name = "id_factura",unique = true,nullable = false)
+    private Long id;
+
+    @Column(name = "fecha",nullable = false)
+    @NotNull(message = "La fecha de la factura no puede ser null")
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime fecha;
+
+    @DecimalMin(value = "0.01",message = "El valor mínimo del monto debe ser 0.01")
+    private Double monto;
+
+    @ManyToOne(fetch = FetchType.LAZY,cascade = CascadeType.MERGE)
+    @JoinColumn(name = "id_persona")
+    private Persona persona;
 }
--- src/main/java/cu/rayner/getechnologiesmx/core/model/Persona.java
+++ src/main/java/cu/rayner/getechnologiesmx/core/model/Persona.java
@@ -1,4 +1,46 @@
 package cu.rayner.getechnologiesmx.core.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table(name = "personas",schema = "public")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Persona {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO,generator = "persona_seq")
+    @SequenceGenerator(name = "persona_seq", sequenceName = "persona_seq_id",initialValue = 1,allocationSize = 1)
+    @Column(name = "id_persona",unique = true,nullable = false)
+    private Long id;
+
+    @Column(name = "nombre",nullable = false,length = 100)
+    @Size(min = 4,max = 100,message = "El nombre de la persona debe tener entre 4 y 100 caracteres")
+    @NotBlank(message = "El nombre de la persona no puede ser nulo o estar vacío")
+    private String nombre;
+
+    @Column(name = "apellido_paterno",nullable = false,length = 100)
+    @Size(min = 4,max = 100,message = "El apellido paterno de la persona debe tener entre 4 y 100 caracteres")
+    @NotBlank(message = "El apellido paterno de la persona no puede ser nulo o estar vacío")
+    private String apellidoPaterno;
+
+    @Transient
+    private String apellidoMaterno;
+
+    // Hola, valido que esta identificación es el de un pasaporte para que así se a nivel global
+    @Column(name = "identificacion",unique = true,nullable = false,length = 50)
+    @Size(min = 6,max = 15,message = "La identificación de la persona debe tener entre 6 y 15 caracteres")
+    @NotBlank(message = "La identificación de la persona no puede ser nulo o estar vacío")
+    private String identificacion;
+
+    @OneToMany(mappedBy = "persona",fetch = FetchType.LAZY,cascade = CascadeType.ALL)
+    private List<Factura> facturas;
+
 }
